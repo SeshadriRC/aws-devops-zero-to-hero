5732928203fd3c8f8276434abe8761236239e533
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-02-19 19:39:25 +0530

    Update ec2-create.sh

--- My-Activities/Scripts/EC2/ec2-create.sh
+++ My-Activities/Scripts/EC2/ec2-create.sh
@@ -1,1 +1,7 @@
-dfd
+aws ec2 run-instances \
+  --image-id ami-051a31ab2f4d498f5 \
+  --instance-type t2.micro \
+  --key-name first-ec2 \
+  --count 1 \
+  --block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":8,"VolumeType":"gp2","DeleteOnTermination":true}}]' \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=first-ec2}]'
