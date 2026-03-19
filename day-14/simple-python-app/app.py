931896d75abae36ddde0d2dda4b8c1a75c349388
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-03-19 15:58:03 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is my new application'
+    return 'Hello, world! This is my second application - i removed inline policy'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
