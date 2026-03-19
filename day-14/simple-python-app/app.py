bde3719c7e11c61128e28f0257075908534ff56d
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-03-19 16:04:13 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is my second application - i removed inline policy'
+    return 'Hello, world! This is my second application - i removed inline policy. Removed AWSCodeDeployDeployerAccess/AWSCodeDeployRole/AmazonS3FullAccess'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
