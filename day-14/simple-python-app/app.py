44369637e4247a3657c32bc560475107c38b79c5
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-03-19 18:07:42 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is my second application-created new codebuild. removed base directory line from buildspec'
+    return 'Hello, world! This is my second application-created new codebuild. removed base directory line from buildspec and created codedeploy stage'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
